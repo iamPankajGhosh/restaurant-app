c045cca18c3d34bc5bc6908ddc92288452c6c2be
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-05-08 00:01:37 +0530

    welcome screen done

---
+++ constants/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    primary: "#ea580c",
+    white: "#fff",
+    black: "#000",
+    grayBg: "#e5e5e5",
+    neutral: (opacity) => `rgba(10, 10, 10, ${opacity})`,
+  },
+  fontWeights: {
+    medium: "500",
+    semibold: "600",
+    bold: "700",
+  },
+  redius: {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+  },
+};
